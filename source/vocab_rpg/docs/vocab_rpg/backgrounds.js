3635611652ebd929ee59a0cf02790e00bf3bd9d4
Kent Rasmussen <hyprkookeez@gmail.com>
2022-03-10 16:19:34 +0900

    Copy JS files to docs folder, too

--- source/vocab_rpg/docs/vocab_rpg/backgrounds.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const backgrounds = {
-    "dirt": {
-        file: "images/backgrounds/dirt.png",
-        name: "dirt",
-    }
-};
