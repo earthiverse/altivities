135f1470daac9af32a1eaf753f054b382e312e42
Kent Rasmussen <hyprkookeez@gmail.com>
2022-03-15 11:27:14 +0900

    Add & update posters

--- docs/bingo/viewportFix.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty("--vh", `${vh}px`);
-window.addEventListener("resize", () => {
-    const vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty("--vh", `${vh}px`);
-});
