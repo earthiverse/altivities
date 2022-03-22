d4f1d527385eff0c6bd8050144ba94ff84e23d4f
Kent Rasmussen <hyprkookeez@gmail.com>
2022-03-22 14:25:07 +0900

    I think it's the network preventing stuff

--- docs/minimalpairs/index.js
+++ docs/minimalpairs/index.js
@@ -11,12 +11,22 @@ async function prepare() {
     const options = {
         debug: 3,
         config: {
-            "iceServers": [
+            iceServers: [
                 {
                     urls: "stun:openrelay.metered.ca:80"
                 },
                 {
                     urls: "turn:openrelay.metered.ca:80",
+                    username: "openrelayproject",
+                    credential: "openrelayproject"
+                },
+                {
+                    urls: "turn:openrelay.metered.ca:443",
+                    username: "openrelayproject",
+                    credential: "openrelayproject"
+                },
+                {
+                    urls: "turn:openrelay.metered.ca:443?transport=tcp",
                     username: "openrelayproject",
                     credential: "openrelayproject"
                 }
